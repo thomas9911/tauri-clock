2ed4112ccc52be423fb0264ff0106b2f60f1aec6
Thomas Timmer <thomas.timmer@bettyblocks.com>
2024-04-29 11:06:34 +0200

    fix: use fixed timezone to fix linux problems

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,20 +4,19 @@
 use tauri::Window;
 use time::macros::format_description;
 use time::OffsetDateTime;
-
+use time_tz::OffsetDateTimeExt;
 
 #[tauri::command]
 fn init_process(window: Window) {
     let format_description = format_description!(version = 2, "[hour]:[minute]:[second]");
+    let tz = time_tz::timezones::get_by_name("Europe/Amsterdam").unwrap();
 
-  std::thread::spawn(move || {
-    loop {
+    std::thread::spawn(move || loop {
         std::thread::sleep(std::time::Duration::from_millis(300));
 
-        let now = OffsetDateTime::now_local().unwrap();
+        let now = OffsetDateTime::now_utc().to_timezone(tz);
         let _ = window.emit("clock", now.format(&format_description).unwrap());
-      }
-  });
+    });
 }
 
 fn main() {
